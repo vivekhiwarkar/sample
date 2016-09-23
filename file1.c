457f92c1d7d9c1bf96a191a779e2706807fae23f
vivekhiwarkar <vivek.hiwarkar@gmail.com>
2016-09-23 20:44:39 +0530

    Show this too at  ese

--- file1.c
+++ file1.c
@@ -11,21 +11,6 @@ this is one more for mse
 Hi This is the first line
 
 
-Hi This is the 3rd line
-
-Hi this is new line for mse
-
-
-this is one more for mse
-
-Hi This is the first line
-
-
-Hi This is the 3rd line
-
-Hi this is new line for mse
-
-
 this is one more for mse
 
 
